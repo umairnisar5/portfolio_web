11e8ee58f11abfe86494ea23ac8b28bd56174240
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-12-08 04:03:59 +0500

    update javascript
    adding other projects

--- script.js
+++ script.js
@@ -2,90 +2,90 @@ const projects = [
     {
       name: "Breakout Game",
       github: "https://github.com/umairnisar5/breakout-game",
-      path: "https://umairnisar5.github.io/Breakout-game/"
+      path: "https://umairnisar5.github.io/breakout-game/"
     },
   
     {
         name: "New Year Countdown",
         github: "https://github.com/umairnisar5/New-Year-Countdown",
-        path: "https://umairnisar5.github.io/Nea-Year-Countdown/"
+        path: "https://umairnisar5.github.io/New-Year-Countdown/"
     },
     {
         name: "Rlaxer",
         github: "https://github.com/umairnisar5/relaxer-app",
-        path:  "https://umairnisar5.github.io/Rlaxer/"
+        path:  "https://umairnisar5.github.io/relaxer-app/"
     },
     {
       name: "Sportable-list",
       github: "https://github.com/umairnisar5/sortable-list",
-      path:  "https://umairnisar5.github.io/Sportable-list/"
+      path:  "https://umairnisar5.github.io/sortable-list/"
   },
   {
       name: "Booking Seat ",
       github: "https://https://github.com/umairnisar5/booking",
-      path:  "https://https://github.com/umairnisar5/booking/"
+      path:  "https://umairnisar5.github.io/booking/"
   },
   {
       name: "Speak-number-gussing",
       github: "https://github.com/umairnisar5/speak-number-gusesing",
-      path:  "https://umairnisar5.github.io/Speak-number-gussing/"
+      path:  "https://umairnisar5.github.io/speak-number-gusesing/"
   },
   {
       name: "Typing-Game",
       github: "https://github.com/umairnisar5/typing-game-",
-      path:  "https://umairnisar5.github.io/Typing-Game/"
+      path:  "https://umairnisar5.github.io/typing-game-/"
   },
   {
       name: "Music-Player",
       github: "https://github.com/umairnisar5/music-player",
-      path:  "https://umairnisar5.github.io/Music-Player/"
+      path:  "https://umairnisar5.github.io/music-player/"
   },
   {
       name: "Meal Finder",
       github: "https://github.com/umairnisar5/meal-finder",
-      path:  "https://umairnisar5.io/Movie-seat-booking/"
+      path:  "https://umairnisar5.github.io/meal-finder/"
   },
   {
       name: "Menu-Slider",
       github: "https://github.com/umairnisar5/menu-slider",
-      path:  "https://umairnisar5.github.io/Model-Menu-Slider/"
+      path:  "https://umairnisar5.github.io/menu-slider/"
   },
   {
       name: "Memory-card",
       github: "https://github.com/umairnisar5/memory-cards",
-      path:  "https://muhammadtalha123.github.io/Memory-card/"
+      path:  "https://umairnisar5.github.io/memory-cards/"
   },
 
   {
       name: "Lyrics-search",
       github: "https://github.com/umairnisar5/lyrics-search",
-      path:  "https://umairnisar5.github.io/Lyrics-search/"
+      path:  "https://umairnisar5.github.io/lyrics-search/"
   },
  
   {
       name: "Hangman-Game",
       github: "https://github.com/umairnisar5/hangman-game1",
-      path:  "https://umairnisar5.github.io/Hangman-Game/"
+      path:  "https://umairnisar5.github.io/hangman-game1/"
   },
   {
-      name: "Net Flix ",
-      github: "https://github.com/umairnisar5/top-five-netflix",
-      path:  "https://umairnisar5.github.io/Expense-Tracker/"
-  },
-  {
-      name: "Expence Tracker",
-      github: "https://github.com/umairnisar5/expense-tracker-react-project",
-      path:  "https://umairnisar5.github.io/Exchange-Rate-Calculator/"
-  },
-  {
-      name: "Web Animations",
+      name: "Red Queen ",
       github: "https://github.com/umairnisar5/red-queen-api",
-      path:  "https:/https://github.com/umairnisar5/red-queen-api/"
+      path:  "https://umairnisar5.github.io/red-queen-api/"
   },
+//   {
+//       name: "Exchance rate",
+//       github: "https://umairnisar5.github.io/Exchange-rate/",
+//       path:  "https://github.com/umairnisar5/Exchange-rate"
+//   },
+//   {
+//       name: "Web Animations",
+//       github: "https://github.com/umairnisar5/red-queen-api",
+//       path:  "https://umairnisar5.github.io/web-animation-api/"
+//   },
   {
-      name: "E-commernce",
-      github: "https://github.com/umairnisar5/e-commerce",
-      path:  "https://github.com/umairnisar5/e-commerce"
+      name: "Login Page",
+      github: "https://github.com/umairnisar5/java1",
+      path:  "https://umairnisar5.github.io/java1/"
   },
   ];
 
